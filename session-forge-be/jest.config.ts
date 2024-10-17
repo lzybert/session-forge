f4d27f2e10acda1fe73405dc239ff70cd7f268da
lzybert <lukasz.zybert@gmail.com>
2024-10-17 14:19:08 +0200

    Initial commit with structure.

---
+++ session-forge-be/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: 'session-forge-be',
+  preset: '../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../coverage/session-forge-be',
+};
